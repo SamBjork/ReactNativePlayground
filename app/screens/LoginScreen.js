9110de656bc036624d4e168a478858d967094e02
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-08-28 16:26:14 +0200

    yup

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,61 +1,67 @@
-import React from 'react'
-import { StyleSheet, Image } from 'react-native'
-import { Formik } from 'formik'
-import * as Yup from 'yup'
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-import Screen from '../components/Screen'
-import AppTextInput from '../components/AppTextInput';
-import AppButton from '../components/AppButton';
+import Screen from "../components/Screen";
+import AppTextInput from "../components/AppTextInput";
+import AppButton from "../components/AppButton";
+import AppText from "../components/AppText";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 export default function LoginScreen() {
-
-    return (
-        <Screen style={styles.container}>
-            <Image
-                source={require("../assets/logo-red.png")}
-                style={styles.logo} />
-            < Formik
-                initialValues={{ email: '', password: '' }}
-                onSubmit={(values => console.log(values))}
-            >
-                {(handleChange, handleSubmit) => (
-                    <>
-                        <AppTextInput
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            icon="email"
-                            keyboardType="email-address"
-                            onChangeText={handleChange("email")}
-                            placeholder="Email"
-                            textContentType="emailAddress"
-                        />
-                        <AppTextInput
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            icon="lock"
-                            keyboardType="default"
-                            onChangeText={handleChange("password")}
-                            placeholder="Password"
-                            secureTextEntry={true}
-                            textContentType="password" />
-                        <AppButton title="login" onPress={handleSubmit} />
-                    </>
-                )}
-            </Formik >
-
-        </Screen>
-    )
+  return (
+    <Screen style={styles.container}>
+      <Image source={require("../assets/logo-red.png")} style={styles.logo} />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        {({ handleChange, handleSubmit, errors }) => (
+          <>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              keyboardType="email-address"
+              onChangeText={handleChange("email")}
+              placeholder="Email"
+              textContentType="emailAddress"
+            />
+            <AppText style={{ color: "red" }}>{errors.email}</AppText>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              keyboardType="default"
+              onChangeText={handleChange("password")}
+              placeholder="Password"
+              secureTextEntry={true}
+              textContentType="password"
+            />
+            <AppText style={{ color: "red" }}>{errors.password}</AppText>
+            <AppButton title="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10
-    },
-    logo: {
-        width: 80,
-        height: 80,
-        alignSelf: "center",
-        marginTop: 50,
-        marginBottom: 20,
-    }
-})
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
